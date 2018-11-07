49c1ea5750b7e17a55711d563d58d1b04627e8d7
Simon L <31842827+Simonprog@users.noreply.github.com>
2018-11-07 14:24:05 +0100

    Customer and and account feature implemented

---
+++ SimonsBankApp/Bank/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimonsBankApp.Bank
+{
+    public class Customer
+    {
+        public string CustomerNo { get; set; }
+        public string Name { get; set; }
+
+        public DateTime BirthYear { get; set; }
+
+        public List<Account> Accounts { get; set; }
+    }
+}
